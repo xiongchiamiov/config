b40b68af267804927f469dd7cb685239c4e7773f
xiongchiamiov <xiong.chiamiov@gmail.com>
2010-01-10 12:06:45 -0800

    fix default layout

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -87,6 +87,9 @@ tags = {}
 for s = 1, screen.count() do
     -- Each screen has its own tag table.
     tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s)
+    for tagnumber = 1, 9 do
+        awful.layout.set(layouts[1], tags[s][tagnumber])
+    end
 end
 -- }}}
 
